933dacf962c6467b81e28d2f4026da69cd828ce0
efraibo <efraibo@gmail.com>
2021-11-27 23:51:48 -0300

    ajutes

--- calculo.js
+++ calculo.js
@@ -1,12 +1,17 @@
 function calculo() {
-    let a = parseInt(document.getElementById("A").value);
-    let b = parseInt(document.getElementById("B").value);
-    let c = parseInt(document.getElementById("C").value);
-    let d = parseInt(document.getElementById("D").value);
+    const qtdTonelada = 3;
+    const valorPorCubo = 0.17;
+
+    let a = parseFloat(document.getElementById("A").value);
+    let b = parseFloat(document.getElementById("B").value);
+    let c = parseFloat(document.getElementById("C").value);
+    let d = parseFloat(document.getElementById("D").value);
 
     let calculoCubo = ((a + b) / 2) * ((c + d) / 2);
-    let valorPago = calculoCubo * (3 * 0.17);
+    let valorPago = calculoCubo * (qtdTonelada * valorPorCubo);
     let valor = valorPago.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-
-    alert(valor);
+       
+    alert("O valor a ser pago é: " + valor);    
 }
+
+
